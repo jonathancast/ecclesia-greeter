759706c9808f177f37cd45bc4a521e05ab0081dc
Jonathan Cast <jonathanccast@fastmail.fm>
2018-04-27 16:52:43 -0500

    Factor the member section out into its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
-
+import { MemberComponent } from './member.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    MemberComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/member.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'member',
+    templateUrl: './member.component.html',
+    styleUrls: ['./member.component.css'],
+    providers: [],
+})
+export class MemberComponent {
+}
